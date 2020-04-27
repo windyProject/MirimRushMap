eae3a72f4d8990a46f583c86b1b17ce4088b7f44
Lee_yam <48716775+Leemainsw@users.noreply.github.com>
2020-04-27 13:40:43 +0900

    메인화면 및 virtual keyboard 추가
    - 버튼 수정 해야함

---
+++ map/Assets/Script/KeyBoard.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeyBoard : MonoBehaviour
+{
+    string word = null;
+    int wordIndex = 0;
+    string alpha;
+    public Text myName = null;
+
+    public void alphabetFunction(string alphabet)
+    {
+        wordIndex++;
+        word = word + alphabet;
+        myName.text = word;
+    }
+
+    public void DeleteFunction()
+    {
+        if (wordIndex != 0)
+        {
+            wordIndex--;
+            word = myName.text.Substring(0, wordIndex);
+            myName.text = myName.text.Substring(0, wordIndex);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
