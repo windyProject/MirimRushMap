69e149af6a46205e94638fe9c46af9be38324ff0
Lee_yam <48716775+Leemainsw@users.noreply.github.com>
2020-04-27 22:09:51 +0900

    카운트 다운 추가(희나댁 !^^) + 메인화면 추가 및 맵 수정

---
+++ map/Assets/Script/CountDown.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CountDown : MonoBehaviour
+{
+    private float timer;
+    private float onetime = 50;
+    public GameObject[] num;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 0;
+        for (int i = 0; i < 5; i++)
+            num[i].SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (timer == 0)
+            Time.timeScale = 0.0f;
+
+        if (timer < onetime * 5)
+        {
+            if (timer >= onetime)
+                num[(int)timer / (int)onetime - 1].SetActive(false);
+            num[(int)timer / (int)onetime].SetActive(true);
+            timer++;
+        }
+        else
+            Time.timeScale = 1.0f;
+    }
+
+}
