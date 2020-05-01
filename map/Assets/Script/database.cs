99619d71fdb36ae32265e36daebfbc9ea2408116
2uijin <49094082+2uijin@users.noreply.github.com>
2020-05-01 22:27:43 +0900

    firebase
    firebase connect

---
+++ map/Assets/Script/database.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using Firebase;
+using Firebase.Unity.Editor;
+using Firebase.Database;
+
+
+public class database : MonoBehaviour { 
+
+    private static database Instance;
+    public DatabaseReference reference;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://mirimrush.firebaseio.com/");
+
+        reference = FirebaseDatabase.DefaultInstance.RootReference;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
