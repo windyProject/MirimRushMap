e07b88acaac48c113e6c6b83968a2f1a5eef116f
Areum1 <s2018s10@e-mirim.hs.kr>
2020-05-10 03:38:23 +0900

    제한시간 추가

---
+++ map/Assets/Script/Timer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    float LimitTime = 91f;
+    public Text TimeText;
+    void Update()
+    {
+        transform.Translate(new Vector3(155, -64, 55));
+
+        LimitTime -= Time.deltaTime;
+
+        if (LimitTime >= 0)
+        {
+            if (LimitTime >= 60f)
+            {
+                int b = (int)LimitTime - 60;
+                if (b < 10)
+                    TimeText.text = "1 : " + "0" + b;
+                else
+                    TimeText.text = "1 : " + b;
+            }
+            else
+            {
+                int b = (int)LimitTime;
+                if (b < 10)
+                    TimeText.text = "0 : 0" + b;
+                else
+                    TimeText.text = "0 : " + b;
+            }
+        }
+
+        else
+        {
+            TimeText.text = "시간 끝남";
+            return;
+        }
+    }
+}
